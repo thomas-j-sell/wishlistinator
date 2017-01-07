ee1e23029e1d42462795e0f5f5d5d7c7d62af4e3
Tom Sell <thomas.j.sell@gmail.com>
2017-01-07 16:27:02 -0500

    add users page, set up proper subscriptions, limit wish page to wishes of current user

--- client/main.js
+++ client/main.js
@@ -1,13 +1,21 @@
 import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
 // import { ReactiveVar } from 'meteor/reactive-var';
 
 // import './main.html';
 
+Meteor.subscribe("userList");
+Meteor.subscribe("wishes");
+
 Template.wishes.helpers({
   wishes() {
     return Wishes.find({});
-  },
+  }
+});
+
+Template.users.helpers({
   users() {
-    return Meteor.users.find({});
+    console.log(Meteor.users.find());
+    return Meteor.users.find();
   }
 });
--- server/main.js
+++ server/main.js
@@ -3,3 +3,11 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 });
+
+Meteor.publish("userList", function() {
+  return Meteor.users.find();
+});
+
+Meteor.publish("wishes", function wishesPublication() {
+  return Wishes.find({author: this.userId});
+});
